dcc5ea3830314e35f1557f6a3272e15b8cf1dd2a
AmBir <amarbiradar147@gmail.com>
2025-02-18 11:30:14 +0530

    cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cors())
 app.use("/", authRouter)
 
