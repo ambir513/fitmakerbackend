7e66d4d3d9bc30fca62dbd51f8b669f9f0c20723
jayeshh298 <officaljayesh5452@gmail.com>
2025-02-20 19:01:49 +0530

    sdded

--- src/assets/Shop.jsx
+++ src/assets/Shop.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import img from './ff1.Webp'
 
 function Shop() {
   return (
@@ -303,6 +304,247 @@ function Shop() {
         </div>
       </div>
     </div>
+
+
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <div class="flex flex-col overflow-hidden rounded-lg bg-gray-900 sm:flex-row md:h-80">
+   
+      <div class="flex w-full flex-col p-4 sm:w-1/2 sm:p-8 lg:w-2/5">
+        <h2 class="mb-4 text-xl font-bold text-white md:text-2xl lg:text-4xl">Summer Sale<br />Up to 70% off.</h2>
+
+        <p class="mb-8 max-w-md text-gray-400">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.</p>
+
+        <div class="mt-auto">
+          <a href="#" class="inline-block rounded-lg bg-white px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base">Save now</a>
+        </div>
+      </div>
+     
+
+      <div class="order-first h-48 w-full bg-gray-700 sm:order-none sm:h-auto sm:w-1/2 lg:w-3/5">
+        <img src = {img} loading="lazy" alt="Photo by Dom Hill" class="h-full w-full object-cover object-center" />
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<div class="font-[sans-serif] bg-gray-100">
+      <div class="p-4 mx-auto lg:max-w-7xl md:max-w-4xl sm:max-w-xl max-sm:max-w-sm">
+        <h2 class="text-2xl sm:text-3xl font-extrabold text-gray-800 mb-6 sm:mb-10">Shop proteins</h2>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-xl:gap-4 gap-6">
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://www.guardian.in/cdn/shop/files/Artboard1_18025de0-e866-4bde-9754-3436880c887e.jpg?v=1734090960&width=800" alt="Product 1"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Sole Elegance</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$10</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://www.guardian.in/cdn/shop/files/Artboard_4_d6a86797-ea51-440d-a644-9942ca69b4d4.png?v=1739966971&width=800" alt="Product 2"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Urban Sneakers</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$12</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://plantigo.in/cdn/shop/files/BrazilianChoc1kg-15_1.png?v=1715636839&width=700" alt="Product 3"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Velvet Boots</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$14</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://s.alicdn.com/@sc04/kf/Hd27b483656b54f42a118c7f76b393b1bC.jpg_720x720q50.jpg" alt="Product 3"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Summit Hiking</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$12</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://img10.hkrtcdn.com/37432/prd_3743149-MuscleBlaze-Biozyme-Performance-Whey-3.85-lb-Triple-Chocolate_o.jpg" alt="Product 3"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Zenith Glow</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$15</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://www.bigmusclesnutrition.com/cdn/shop/files/goldwheyinformedchoice-09-removebg-preview_360x.png?v=1719816486" alt="Product 3"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Echo Elegance</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$14</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://images-cdn.ubuy.co.in/64396160db5e3d3ee762aaa6-per4m-protein-whey-powder-67-servings.jpg" alt="Product 3"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Pumps</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$14</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded p-4 cursor-pointer hover:-translate-y-1 transition-all relative">
+            <div class="mb-4 bg-gray-100 rounded p-2">
+              <img src="https://www.bodybuildingindia.com/cdn/shop/products/Muscle-Mantra-Pro-Series-Best-Whey-XT-Muscle-Mantra-42799834.jpg?v=1739108505&width=600" alt="Product 3"
+                class="aspect-[33/35] w-full object-contain" />
+            </div>
+
+            <div>
+              <div class="flex gap-2">
+                <h5 class="text-base font-bold text-gray-800">Blaze Burst</h5>
+                <h6 class="text-base text-gray-800 font-bold ml-auto">$14</h6>
+              </div>
+              <p class="text-gray-500 text-[13px] mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div class="flex items-center gap-2 mt-4">
+                <div
+                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block" viewBox="0 0 64 64">
+                    <path
+                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </div>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
     </div>
   )
 }
