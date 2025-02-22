055193f9f2777e63edf92c53f36d7f7af498a432
jayeshh298 <officaljayesh5452@gmail.com>
2025-02-22 17:30:27 +0530

    added

--- src/assets/Shop.jsx
+++ src/assets/Shop.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import img from './ff1.Webp'
+import ff1 from "../assets/ff1.webp"; // Adjust path if needed
+
 
 function Shop() {
   return (
@@ -41,7 +42,7 @@ function Shop() {
                   <path
                     d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
                     data-original="#000000"></path>
-                </svg>
+                </svg>  
               </button>
               <button type="button" title="Add to cart" class="bg-transparent outline-none border-none">
                 <svg xmlns="http://www.w3.org/2000/svg" class="fill-gray-800 w-5 h-5 inline-block" viewBox="0 0 512 512">
@@ -322,7 +323,7 @@ function Shop() {
      
 
       <div class="order-first h-48 w-full bg-gray-700 sm:order-none sm:h-auto sm:w-1/2 lg:w-3/5">
-        <img src = {img} loading="lazy" alt="Photo by Dom Hill" class="h-full w-full object-cover object-center" />
+        <img src = {ff1 } loading="lazy" alt="Photo by Dom Hill" class="h-full w-full object-cover object-center" />
       </div>
       
     </div>
